11015fe355f9786a8acbf3a5686d640a75e52191
mmironov <miroslavmironov@gmail.com>
2013-03-07 21:11:57 +0200

    Function log() implemented.

--- baggage.cpp
+++ baggage.cpp
@@ -1,5 +1,10 @@
 #include "baggage.h"
+#include <iostream>
+using namespace std;
 
 void log(Baggage b)
 {
+    cout << "Owner: " << b.owner << endl;
+    cout << "Flight number: " << b.flightNumber << endl;
+    cout << "Weight: " << b.weight << endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "baggage.h"
+#include <cstring>
+using namespace std;
 
 int main()
 {
+	Baggage b;
+	b.flightNumber = 1004;
+	b.weight = 19;
+	strcpy(b.owner, "Mironov");
+
+	log(b);
+
     return 0;
 }
