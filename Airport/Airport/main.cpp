3fa4206d37f57d7ecc8c3c124a49d20092f463c2
Miroslav Mironov <mmironov@android_3c5faa5388145042.nl.ad.virtual-affairs.com>
2013-04-11 17:45:58 +0300

    Added new project files.

---
+++ Airport/Airport/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Airport
+//
+//  Created by Miroslav Mironov on 4/11/13.
+//  Copyright (c) 2013 Miroslav Mironov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
